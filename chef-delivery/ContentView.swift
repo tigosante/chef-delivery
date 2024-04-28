afffe6ae05f0ba096ea42dd11751d9928d15957f
Tiago Silva <tsilvasantos38@gmail.com>
2024-04-28 15:06:44 -0300

    Initial Commit

---
+++ chef-delivery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chef-delivery
+//
+//  Created by Tiago Silva on 28/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chef-delivery/chef_deliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  chef_deliveryApp.swift
+//  chef-delivery
+//
+//  Created by Tiago Silva on 28/04/24.
+//
+
+import SwiftUI
+
+@main
+struct chef_deliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
