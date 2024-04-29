c9dfdfba82a973bbeb06d527f4ecad4c6891cc7f
Tiago Silva <tsilvasantos38@gmail.com>
2024-04-29 12:06:08 -0300

    feature: add images in new folders and add details view

---
+++ chef-delivery/Components/StoresView/StoreDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  StoreDetailsView.swift
+//  chef-delivery
+//
+//  Created by Tiago Silva on 29/04/24.
+//
+
+import SwiftUI
+
+struct StoreDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoreDetailsView()
+}
