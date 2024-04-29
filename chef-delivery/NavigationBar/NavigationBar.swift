ec3ce89a9dc48931ae02354314a72aaa45e10fe8
Tiago Silva <tsilvasantos38@gmail.com>
2024-04-29 09:40:06 -0300

    feature: add NavigationBar with style

---
+++ chef-delivery/NavigationBar/NavigationBar.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationBar.swift
+//  chef-delivery
+//
+//  Created by Tiago Silva on 28/04/24.
+//
+
+import SwiftUI
+
+struct NavigationBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationBar()
+}
