108c91f62802f55d0169768d54b9563203813b56
Tiago Silva <tsilvasantos38@gmail.com>
2024-05-04 10:47:51 -0300

    feature: finish functionalities

--- chef-delivery/Components/StoresView/StoresContainerView.swift
+++ chef-delivery/Components/StoresView/StoresContainerView.swift
@@ -10,21 +10,62 @@ import SwiftUI
 struct StoresContainerView: View {
     let stores: [StoreType]
     let title = "Lojas"
+    @State private var ratingFilter = 0
+    var filteredStores: [StoreType] {
+        return stores.filter({ store in store.stars >= ratingFilter })
+    }
     
     var body: some View {
         VStack(alignment: .leading) {
-            Text(title)
-                .font(.headline)
-            
-            VStack(alignment: .leading, spacing: 30) {
-                ForEach(stores, id: \.id) { store in
-                    NavigationLink {
-                        StoreDetailsView(store: store)
+            HStack {
+                Text(title)
+                    .font(.headline)
+                
+                Spacer()
+                
+                Menu("Filtrar") {
+                    Button {
+                        if ratingFilter > 0 {
+                            ratingFilter = 0
+                        }
                     } label: {
-                        StoreItemView(store: store)
+                        Text("Limpar filtro")
+                            .foregroundColor(.black)
                     }
-                    .foregroundColor(.black)
                     
+                    Divider()
+                    
+                    ForEach(1...5, id: \.self) { star in
+                        Button {
+                            ratingFilter = star
+                        } label: {
+                            let starLabel = star == 1 ? "estrela" : "estrelas"
+                            let sufixLabel = star == 5 ? "" : "ou mais"
+                            Text("\(star) \(starLabel) \(sufixLabel)")
+                                .foregroundColor(.black)
+                        }
+                    }
+                }
+                .foregroundColor(.black)
+            }
+            
+            if filteredStores.isEmpty {
+                Text("Nenhum resultado encontrado.")
+                    .foregroundColor(.accentColor)
+                    .font(.title2)
+                    .bold()
+                    .padding(.vertical, 32)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .center)
+            } else {
+                VStack(alignment: .leading, spacing: 30) {
+                    ForEach(filteredStores) { store in
+                        NavigationLink {
+                            StoreDetailsView(store: store)
+                        } label: {
+                            StoreItemView(store: store)
+                        }
+                        .foregroundColor(.black)
+                    }
                 }
             }
         }
